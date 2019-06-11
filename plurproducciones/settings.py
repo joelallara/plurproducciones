1378098a537ea76374055411963222dfecc994d2
Joel Allara <joel_allara@hotmail.com>
2019-06-10 22:09:57 -0300

    first commit 6

--- plurproducciones/settings.py
+++ plurproducciones/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c^rse1=u4@cpa*x#7tlx#ede$9()b&+_!s40a0sp5kyi#j#axr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition

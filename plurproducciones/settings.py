fe2fcd3cc7a329f407855bd615b6b406f364658e
Joel Allara <joel_allara@hotmail.com>
2019-06-10 22:17:48 -0300

    first commit 7

--- plurproducciones/settings.py
+++ plurproducciones/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 TIME_ZONE = 'America/Argentina/Buenos_Aires'
 LANGUAGE_CODE = 'es'
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = ( os.path.join('static'), )
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
